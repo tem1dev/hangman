aabac171fdf44eef58c88049bfe657eb54711e19
fearux <bekov.tembulatt@gmail.com>
2024-09-29 11:19:11 +0300

    feat: add renderer of each move

---
+++ src/hangman/renderer.py
@@ -0,0 +1,79 @@
+class Renderer:
+    """Responsible for rendering information about the game in the console.
+
+    Attributes:
+        __CONSOLE_HANGMAN_PICTURES: List of sprites of various hangman states.
+    """
+    __CONSOLE_HANGMAN_PICTURES: list[str] = [
+        """ 
+        ----------
+        |        |
+        |        
+        |
+        |
+        |_________ 
+        """,
+        """ 
+        ----------
+        |        |
+        |        0
+        |
+        |
+        |_________ """,
+        """ 
+        ----------
+        |        |
+        |        0
+        |        |
+        |
+        |_________ """,
+        """ 
+        ----------
+        |        |
+        |        0/
+        |        |
+        |
+        |_________ """,
+        """
+        ----------
+        |        |
+        |       \\0/
+        |        |
+        |
+        |_________ """,
+        """
+        ----------
+        |        |
+        |       \\0/
+        |        |
+        |       /
+        |_________ """,
+        """
+        ----------
+        |        |
+        |       \\0/
+        |        |
+        |       / \\
+        |_________ """,
+    ]
+
+    def render(self, attempts: int, word: str, user_letters: set[str]) -> None:
+        """
+        Renders information to the player every turn.
+
+        :return: None.
+        """
+        print(self.get_sprite_for_hangman(attempts))
+        print(f"Текущее слово {word}\n"
+              f"Попытка: {attempts}\n"
+              f"Использованные буквы: {user_letters}")
+
+    def get_sprite_for_hangman(self, attempts: int) -> str:
+        """
+        Returns a specific hangman's sprite depending on the player's condition
+        (number of attempts).
+
+        :param attempts: The number of attempts made by the player
+        :return: Hangman's sprite.
+        """
+        return self.__CONSOLE_HANGMAN_PICTURES[attempts]

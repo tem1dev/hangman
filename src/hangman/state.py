5a0bc785597e5f76482521f15417d573f0ef3a4a
fearux <bekov.tembulatt@gmail.com>
2024-10-02 20:35:50 +0300

    fix: create init method with render and valid letters

--- src/hangman/state.py
+++ src/hangman/state.py
@@ -1,16 +1,14 @@
-from typing import Final
-
-IS_LOSE_STATE: Final[int] = 6
-
-
 class State:
     """Contains predicate methods that check a certain state of the game."""
+
+    def __init__(self, lose_state):
+        self._lose_state = lose_state
 
     def is_game_won(self, secret_word: str, player_word: str) -> bool:
         return secret_word == player_word
 
     def is_game_over(self, attempts: int) -> bool:
-        return attempts == IS_LOSE_STATE
+        return attempts == self._lose_state
 
     def is_finish_game(self, line: str) -> bool:
         return line.startswith("н")
